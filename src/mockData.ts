81bb034e169ee9160a759342f51191d43973bf2c
Ruslan <ruslanchikbest@gmail.com>
2026-08-03 10:37:15 +0300

    feat: add types and mock data, clean up default vite styles

---
+++ src/mockData.ts
@@ -0,0 +1,67 @@
+import type { Order, Product } from './types';
+
+export const products: Product[] = [
+  {
+    id: 1,
+    serialNumber: 12345671,
+    isNew: true,
+    photo: 'https://via.placeholder.com/60',
+    title: 'Gigabyte Technology X58-USB3 (Socket 1366)',
+    type: 'Monitors',
+    specification: 'Specification 1',
+    guarantee: { start: '2017-04-06 00:00:00', end: '2025-08-06 00:00:00' },
+    price: [
+      { value: 2500, symbol: 'USD', isDefault: false },
+      { value: 250000.5, symbol: 'UAH', isDefault: true },
+    ],
+    order: 1,
+    date: '2017-09-06 12:09:33',
+  },
+  {
+    id: 2,
+    serialNumber: 12345672,
+    isNew: false,
+    photo: 'https://via.placeholder.com/60',
+    title: 'Gigabyte Technology X58-USB3 (Socket 1366)',
+    type: 'Keyboards',
+    specification: 'Specification 2',
+    guarantee: { start: '2017-04-06 00:00:00', end: '2025-08-06 00:00:00' },
+    price: [
+      { value: 100, symbol: 'USD', isDefault: false },
+      { value: 2600, symbol: 'UAH', isDefault: true },
+    ],
+    order: 1,
+    date: '2017-09-06 12:09:33',
+  },
+  {
+    id: 3,
+    serialNumber: 12345673,
+    isNew: true,
+    photo: 'https://via.placeholder.com/60',
+    title: 'Gigabyte Technology X58-USB3 (Socket 1366)',
+    type: 'Monitors',
+    specification: 'Specification 3',
+    guarantee: { start: '2017-04-06 00:00:00', end: '2025-08-06 00:00:00' },
+    price: [
+      { value: 50, symbol: 'USD', isDefault: false },
+      { value: 1350, symbol: 'UAH', isDefault: true },
+    ],
+    order: 2,
+    date: '2017-06-06 12:09:33',
+  },
+];
+
+export const orders: Order[] = [
+  {
+    id: 1,
+    title: 'Длинное предлинное длиннючее название прихода',
+    date: '2017-04-06 12:09:33',
+    description: 'desc',
+  },
+  {
+    id: 2,
+    title: 'Длинное название прихода',
+    date: '2017-09-06 12:09:33',
+    description: 'desc',
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export interface Price {
+  value: number;
+  symbol: 'USD' | 'UAH';
+  isDefault: boolean;
+}
+
+export interface Guarantee {
+  start: string;
+  end: string;
+}
+
+export interface Product {
+  id: number;
+  serialNumber: number;
+  isNew: boolean;
+  photo: string;
+  title: string;
+  type: string;
+  specification: string;
+  guarantee: Guarantee;
+  price: Price[];
+  order: number; 
+  date: string;
+}
+
+export interface Order {
+  id: number;
+  title: string;
+  date: string;
+  description: string;
+}
